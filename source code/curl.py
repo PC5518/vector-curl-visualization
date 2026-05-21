ca7e3ef8f9b58c5de33b813b1ddf5c778ed18a97
Aditya Narayan Singh <adityansdsdc@outlook.com>
2026-05-21 13:17:58 +0530

    Update curl.py

--- source code/curl.py
+++ source code/curl.py
@@ -5,6 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.animation as animation
 # Attention to the users and readers: this code has been gone through several bug reduction system like claude etc. However, I have not maded optimizing of memory as the primary goal of the project.
+# If the AIM is to focus on memory optimization then native c or c++ is the best way to resolve it.
 class FieldVisualizer:
     def __init__(self):
         # --- Basic Setup ---
